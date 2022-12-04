79a0b4aaa27ac08f805a744c3c05c2a0778b5880
yang <oseau@me.com>
2022-12-05 05:14:37 +0800

    2022 day 4

---
+++ 2022/day/4/part1.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+
+def load():
+    with Path(__file__).with_name("input.txt").open("r") as file:
+        lines = [l.rstrip() for l in file]
+    return lines
+
+
+def main():
+    total = 0
+    for l in load():
+        pair = sorted(
+            [[int(s) for s in pair.split("-")] for pair in l.split(",")],
+            key=lambda p: [p[0], -p[1]],
+        )
+        if pair[0][1] >= pair[1][1]:
+            total += 1
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
